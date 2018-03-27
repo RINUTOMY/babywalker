af9cd552d739c847f1eb6d13633599a3e313894f
RINU TOMY <rinutomy5@gmail.com>
2018-03-27 17:55:36 +0530

    video streaming

---
+++ AndroidStudioProjects/MyApp/app/src/main/java/com/example/hp/myapp/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.hp.myapp;
+
+public class AppConfig {
+	public static final String STREAM_URL = "rtsp://192.168.225.101:1935/live/android_test";
+	public static final String PUBLISHER_USERNAME = "baby";
+	public static final String PUBLISHER_PASSWORD = "babywalker";
+}
